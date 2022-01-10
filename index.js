2aaa16f37d5687936f8aef3925292de6c47a03ce
Kaarthikeya <kaarthikeya1@gmail.com>
2022-01-10 17:28:08 +0530

    \new note button is working!!! Lets go

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-var x = document.getElementsByClassName("box_items_flex");
+/* For adding new things to our new note, right! 
+	Step 1: Append the child on a normal basis;
+	Step 2: Put that in a function;
+	Step 3: Enjoy!
+*/
 
-alert("Hello");
+var boxes = document.querySelector(".box_items");
+boxes.onclick = () => {
+	var classNameForOldBox = "box_items_flex";
+	var newBox = document.createElement("div");
+	var textForNewBox = document.querySelector(".box_items").textContent;
+	newBox.className = "box_items";
+	newBox.append(textForNewBox);
+
+	document.querySelector(".box_items_flex").append(newBox);
+};
