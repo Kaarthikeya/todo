fd2f5c94f7e306169b68e55789b03cb913af6090
Kaarthikeya <kaarthikeya1@gmail.com>
2022-01-29 23:02:29 +0530

    delete and edit button updated

--- script.js
+++ script.js
@@ -1,93 +1,156 @@
 var overlay = document.getElementById("overlay_add");
-
-/* For opening Overlay for adding a new note */
-
 function startOverlay() {
 	overlay.style.transform = "scale(1) translate(-50%, -50%)";
 }
-
-//For closing the opened overlay//
-
 function closeOverlay() {
 	overlay.style.transform = "scale(0) translate(-50%, -50%)";
 }
+function getRandInt() {
+	return Math.floor(Math.random() * 999999999999999) + 1;
+}
 
-const arrayOfNotes = [];
-var idForElements = 0;
+var number = localStorage.length;
+if (number != null) {
+	for (var i = 0; i < number; i++) {
+		const idNumber = localStorage.key(i);
+		console.log(idNumber);
+		//CReating new div elements to suit the needs;
+		var newNote = document.createElement("div");
+		var checkBox = document.createElement("div");
+		var noteInfo = document.createElement("div");
+		var titleFinal = document.createElement("div");
+		var checkBoxClick = document.createElement("input");
+		var editButton = document.createElement("div");
+		var deleteButton = document.createElement("div");
+		var editfa = document.createElement("i");
+		var deletefa = document.createElement("i");
+
+		// GIving them class names;
+		newNote.className = "box";
+		newNote.id = localStorage.key(i);
+		checkBox.className = "checkbox";
+		noteInfo.className = "note_info";
+		titleFinal.className = "title";
+		editButton.className = "edit";
+		deleteButton.className = "delete";
+		deletefa.classList.add("fas", "fa-trash");
+		editfa.classList.add("fas", "fa-edit");
+
+		/* Check Box click functions and stuff!! */
+		checkBoxClick.className = "checkbox_input_for_deleting";
+		checkBoxClick.type = "checkbox";
+
+		//Appending Stuff
+		newNote.appendChild(checkBox);
+		newNote.appendChild(noteInfo);
+		noteInfo.appendChild(titleFinal);
+		noteInfo.appendChild(editButton);
+		noteInfo.appendChild(deleteButton);
+		checkBox.append(checkBoxClick);
+		editButton.appendChild(editfa);
+		deleteButton.appendChild(deletefa);
+		//Giving their text to them;
+		newNote.style.display = "flex";
+
+		titleFinal.textContent = localStorage.getItem(localStorage.key(i));
+
+		var container = document.getElementsByClassName("notes_container")[0];
+		container.appendChild(newNote);
+
+		deleteButton.addEventListener("click", function () {
+			document.getElementById(idNumber).style.display = "none";
+
+			localStorage.removeItem(idNumber);
+		});
+
+		checkBox.addEventListener("click", function () {
+			document.getElementById(idNumber).classList.toggle("line-through");
+		});
+
+		editButton.addEventListener("click", function () {
+			startOverlay();
+			document.getElementById("note_title").value =
+				this.previousSibling.textContent;
+			this.parentNode.parentNode.style.display = "none";
+			localStorage.removeItem(this.parentNode.parentNode.id);
+		});
+	}
+}
 
 function addNewNote() {
 	/* GRabbing the text from the input */
 
 	var titleText = document.getElementById("note_title").value;
-	var additionalText = document.getElementById(
-		"additional_info_for_adding_a_new_note"
-	).value;
 
 	/* Making sure that title is not empty!! */
 	if (!titleText) {
-		alert("Give a title idiot!!! Hate you!!");
+		alert("Give a title idiot!!!");
 		return;
 	}
-
-	/* Working with arrays kids...lets go!! */
-	arrayOfNotes.push({
-		id: idForElements,
-		titleName: titleText,
-		addText: additionalText,
-	});
+	const randint = getRandInt();
 
 	//CReating new div elements to suit the needs;
 	var newNote = document.createElement("div");
 	var checkBox = document.createElement("div");
 	var noteInfo = document.createElement("div");
 	var titleFinal = document.createElement("div");
-	var addFinal = document.createElement("div");
 	var checkBoxClick = document.createElement("input");
 	var editButton = document.createElement("div");
+	var deleteButton = document.createElement("div");
 	var editfa = document.createElement("i");
+	var deletefa = document.createElement("i");
 
 	// GIving them class names;
 	newNote.className = "box";
-	newNote.id = idForElements;
+	newNote.id = randint;
 	checkBox.className = "checkbox";
 	noteInfo.className = "note_info";
 	titleFinal.className = "title";
-	addFinal.className = "additional_info";
 	editButton.className = "edit";
+	deleteButton.className = "delete";
+	deletefa.classList.add("fas", "fa-trash");
 	editfa.classList.add("fas", "fa-edit");
 
 	/* Check Box click functions and stuff!! */
 
 	checkBoxClick.type = "checkbox";
+	checkBoxClick.className = "checkbox_input_for_deleting";
 
 	//Appending Stuff
 	newNote.appendChild(checkBox);
 	newNote.appendChild(noteInfo);
-	newNote.appendChild(editButton);
 	noteInfo.appendChild(titleFinal);
-	noteInfo.appendChild(addFinal);
+	noteInfo.appendChild(editButton);
+	noteInfo.appendChild(deleteButton);
 	checkBox.append(checkBoxClick);
 	editButton.appendChild(editfa);
+	deleteButton.appendChild(deletefa);
 	//Giving their text to them;
 	titleFinal.textContent = titleText;
-	addFinal.textContent = additionalText;
 	newNote.style.display = "flex";
-	idForElements++;
 
 	var container = document.getElementsByClassName("notes_container")[0];
 	container.appendChild(newNote);
 
-	/* CheckBox Stuff */
+	localStorage.setItem(randint, titleText);
+
+	deleteButton.addEventListener("click", function () {
+		document.getElementById(randint).style.display = "none";
+		localStorage.removeItem(randint);
+	});
 
 	checkBox.addEventListener("click", function () {
-		var checky = this.children[0];
-		if (checky.checked) {
-			newNote.style.filter = "opacity(0.3)";
-			newNote.style.textDecoration = "line-through";
-		}
+		document.getElementById(randint).classList.toggle("line-through");
+	});
+
+	editButton.addEventListener("click", function () {
+		startOverlay();
+		document.getElementById("note_title").value =
+			this.previousSibling.textContent;
+		this.parentNode.parentNode.style.display = "none";
+		localStorage.removeItem(this.parentNode.parentNode.id);
 	});
 
 	document.getElementById("note_title").value = "";
-	document.getElementById("additional_info_for_adding_a_new_note").value = "";
 	closeOverlay();
 }
